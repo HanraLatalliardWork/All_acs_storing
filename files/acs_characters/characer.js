fe0797072363f53486035f7daa3981638c53ef92
Gitlab_test_project <letellier.henry@gmail.com>
2021-06-04 17:47:14 +0200

    did a bit of listing

---
+++ files/acs_characters/characer.js
@@ -0,0 +1,15 @@
+function sum(){
+	var a=0,
+	b=5,
+	c=[a+b,b+a,b/a,a/b,a*b,b*a];
+	document.write(a);
+	document.write(b);
+	document.write(c);
+	document.write(c[0]);
+	document.write(c[1]);
+	document.write(c[2]);
+	document.write(c[3]);
+	document.write(c[4]);
+	document.write(c[5]);
+}
+sum();
